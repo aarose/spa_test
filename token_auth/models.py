09dc8d4827af0f72ef5c8d068663a096c8b0334d
aarose <aarose@users.noreply.github.com>
2015-07-28 16:04:08 -0400

    Added timeout for tokens

--- token_auth/models.py
+++ token_auth/models.py
@@ -6,6 +6,9 @@ from google.appengine.ext import ndb
 from users.models import User
 
 
+TOKEN_TIMEOUT = 7 * 24 * 60 * 60  # a week, in seconds
+
+
 class Token(ndb.Model):
     user = ndb.StructuredProperty(User)
     token = ndb.StringProperty()
@@ -20,3 +23,7 @@ class Token(ndb.Model):
         if not self.token:
             self.token = self.generate_token()
         return super(Token, self).put(*args, **kwargs)
+
+    def is_valid(self, timeout=TOKEN_TIMEOUT):
+        """ Returns False if the timeout amount has passed since creation. """
+        pass
